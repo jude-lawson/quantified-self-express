a672d261053d426ac11f751e9cff6441fab21f47
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 13:01:13 -0600

    Uses awaits to properly order record insertions in seeds file since specifying the id was preventing auto-increments

--- db/seeds/main.js
+++ db/seeds/main.js
@@ -22,44 +22,37 @@ exports.seed = async (knex, Promise) => {
     knex('foods').del(),
     knex.raw('ALTER SEQUENCE foods_id_seq RESTART WITH 1')
   ]);
-  
-  // Create foods
-  await Promise.all([
-    knex.raw(`INSERT INTO foods (name, calories)
-              VALUES (?,?)
-              RETURNING id`, [foods[0].name, foods[0].calories]),
-    knex.raw(`INSERT INTO foods (name, calories)
-              VALUES (?,?)
-              RETURNING id`, [foods[1].name, foods[1].calories]),
-    knex.raw(`INSERT INTO foods (name, calories)
-              VALUES (?,?)
-              RETURNING id`, [foods[2].name, foods[2].calories]),
-    knex.raw(`INSERT INTO foods (name, calories)
-              VALUES (?,?)
-              RETURNING id`, [foods[3].name, foods[3].calories]),
-  ]);
+
+  await knex.raw(`INSERT INTO foods (name, calories)
+                  VALUES (?,?)
+                  RETURNING id`, [foods[0].name, foods[0].calories]);
+  await knex.raw(`INSERT INTO foods (name, calories)
+                  VALUES (?,?)
+                  RETURNING id`, [foods[1].name, foods[1].calories]);
+  await knex.raw(`INSERT INTO foods (name, calories)
+                  VALUES (?,?)
+                  RETURNING id`, [foods[2].name, foods[2].calories]);
+  await knex.raw(`INSERT INTO foods (name, calories)
+                  VALUES (?,?)
+                  RETURNING id`, [foods[3].name, foods[3].calories]);
 
   // Create meals
-  await Promise.all([
-    knex.raw(`INSERT INTO meals (name)
-              VALUES (?)`, [meals[0].name]),
-    knex.raw(`INSERT INTO meals (name)
-              VALUES (?)`, [meals[1].name]),
-    knex.raw(`INSERT INTO meals (name)
-              VALUES (?)`, [meals[2].name]),
-    knex.raw(`INSERT INTO meals (name)
-              VALUES (?)`, [meals[3].name])
-  ]);
+  await knex.raw(`INSERT INTO meals (name)
+                  VALUES (?)`, [meals[0].name]);
+  await knex.raw(`INSERT INTO meals (name)
+                  VALUES (?)`, [meals[1].name]);
+  await knex.raw(`INSERT INTO meals (name)
+                  VALUES (?)`, [meals[2].name]);
+  await knex.raw(`INSERT INTO meals (name)
+                  VALUES (?)`, [meals[3].name]);
 
   // Create meal_foods
-  await Promise.all([
-    knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
-              VALUES (?,?)`, [meal_foods[0].meal_id, meal_foods[0].food_id]),
-    knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
-              VALUES (?,?)`, [meal_foods[1].meal_id, meal_foods[1].food_id]),
-    knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
-              VALUES (?,?)`, [meal_foods[2].meal_id, meal_foods[2].food_id]),
-    knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
-              VALUES (?,?)`, [meal_foods[3].meal_id, meal_foods[3].food_id]),
-  ]);
+  await knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                  VALUES (?,?)`, [meal_foods[0].meal_id, meal_foods[0].food_id]);
+  await knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                  VALUES (?,?)`, [meal_foods[1].meal_id, meal_foods[1].food_id]);
+  await knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                  VALUES (?,?)`, [meal_foods[2].meal_id, meal_foods[2].food_id]);
+  await knex.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                  VALUES (?,?)`, [meal_foods[3].meal_id, meal_foods[3].food_id]);
 };
