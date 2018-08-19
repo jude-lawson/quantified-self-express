fbbc4a6d2e1331130c2773f95bf0cfdd555b55ea
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 13:58:45 -0600

    Updates food_request spec to utilize async await

--- test/food_requests.spec.js
+++ test/food_requests.spec.js
@@ -3,114 +3,69 @@ import fetch from 'node-fetch'
 
 import foods from '../fixtures/for_tests/foods';
 import food from '../fixtures/for_tests/food';
-import seed from '../db/seeds/main.js'
 
 describe('Food Requests', () => {
   context('GET /api/v1/foods', () => {
-    it('should return all foods in the database', done => {
-      fetch('http://localhost:8000/api/v1/foods')
-        .then(response => response.json())
-        .then(parsedResponse => {
-          expect(parsedResponse).to.deep.eq(foods)
-          done();
-        })
-        .catch(error => console.error(error))
+    it('should return all foods in the database', async () => {
+      let response       = await fetch('http://localhost:8000/api/v1/foods');
+      let parsedResponse = await response.json();
+
+      expect(response.status).to.eq(200);
+      expect(parsedResponse).to.deep.eq(foods);
     });
   });
 
   context('GET /api/v1/foods/:id', () => {
-    it('should return a single food with the specified id', done => {
-      fetch('http://localhost:8000/api/v1/foods/1')
-        .then(response => response.json())
-        .then(parsedResponse => {
-          expect(parsedResponse).to.deep.eq(food)
-          done();
-        })
-        .catch(error => console.error(error))
+    it('should return a single food with the specified id', async () => {
+      let response       = await fetch('http://localhost:8000/api/v1/foods/1');
+      let parsedResponse = await response.json();
+
+      expect(response.status).to.eq(200);
+      expect(parsedResponse).to.deep.eq(food);
     });
   });
 
   context('POST /api/v1/foods', () => {
-    it('given a food object, it should create a food', done => {
-      let new_food = JSON.stringify({ food: { name: 'Dumplings', calories: 900 } });
+    it('given a food object, it should create a food', async () => {
+      let new_food   = JSON.stringify({ food: { name: 'Dumplings', calories: 900 } });
+      let fetch_init = { method: 'post', body: new_food, headers: { 'Content-Type': 'application/json' } };
 
-      fetch('http://localhost:8000/api/v1/foods', {
-        method: 'post',
-        body: new_food,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
-        expect(response.status).to.eq(200);
-        return response.json()
-      })
-      .then(parsedResponse => {
-        expect(parsedResponse).to.deep.eq({ id: 5, name: 'Dumplings', calories: 900 })
-        done();
-      })
-      .catch(error => console.error(error))
+      let response = await fetch('http://localhost:8000/api/v1/foods', fetch_init);
+      let parsedResponse = await response.json();
+
+      expect(response.status).to.eq(200);
+      expect(parsedResponse).to.deep.eq({ id: 5, name: 'Dumplings', calories: 900 })
     })
 
-    it('should return 400 if the creation was not successful', done => {
-      let new_food = JSON.stringify({ food: {} });
+    it('should return 400 if the creation was not successful', async () => {
+      let errant_food = JSON.stringify({ food: {} });
+      let fetch_init  = { method: 'post', body: errant_food, headers: { 'Content-Type': 'application/json' } };
 
-      fetch('http://localhost:8000/api/v1/foods', {
-        method: 'post',
-        body: new_food,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
-        expect(response.status).to.eq(400);
-        done();
-      })
-      .catch(error => console.error(error))
+      let response       = await fetch('http://localhost:8000/api/v1/foods', fetch_init); 
+
+      expect(response.status).to.eq(400);
     });
 
-    it('should return 400 with error message if \'name\' is not included', done => {
+    it('should return 400 with error message if \'name\' is not included', async () => {
       let errant_food = JSON.stringify({ food: { calories: 900 } });
+      let fetch_init  = { method: 'post', body: errant_food, headers: { 'Content-Type': 'application/json' } };
 
-      fetch('http://localhost:8000/api/v1/foods', {
-        method: 'post',
-        body: errant_food,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
-        expect(response.status).to.eq(400);
-        return response.json();
-      })
-      .then(parsedResponse => {
-        expect(parsedResponse).to.deep.eq({ error: 'Name attribute is required' })
-        done();
-      })
-      .catch(error => console.error(error))
+      let response       = await fetch('http://localhost:8000/api/v1/foods', fetch_init)
+      let parsedResponse = await response.json();
 
+      expect(response.status).to.eq(400);
+      expect(parsedResponse).to.deep.eq({ error: 'Name attribute is required' })
     });
 
-    it('should return 400 with error message if \'calories\' is not included', done => {
+    it('should return 400 with error message if \'calories\' is not included', async () => {
       let errant_food = JSON.stringify({ food: { name: 'Dumplings' } });
+      let fetch_init = { method: 'post', body: errant_food, headers: { 'Content-Type': 'application/json' } };
 
-      fetch('http://localhost:8000/api/v1/foods', {
-        method: 'post',
-        body: errant_food,
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
-        expect(response.status).to.eq(400);
-        return response.json();
-      })
-      .then(parsedResponse => {
-        expect(parsedResponse).to.deep.eq({ error: 'Calories attribute is required' })
-        done();
-      })
-      .catch(error => console.error(error))
+      let response       = await fetch('http://localhost:8000/api/v1/foods', fetch_init)
+      let parsedResponse = await response.json() 
 
+      expect(response.status).to.eq(400);
+      expect(parsedResponse).to.deep.eq({ error: 'Calories attribute is required' })
     });
   });
 });
