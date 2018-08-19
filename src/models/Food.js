1f91f598f273b8f350ef6aeae11d2bebda02a4d6
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 10:35:11 -0600

    Implements Food#show endpoint for getting a single food specified by id

--- src/models/Food.js
+++ src/models/Food.js
@@ -4,7 +4,17 @@ const database = require('knex')(configuration);
 
 class Food  {
   static async getAllFoods(request, response) {
-    return await database('foods').select()
+    return await database('foods').select();
+  }
+
+  static async getSingleFood(food_id) {
+    try {
+      let food = await database.raw(`SELECT foods.* FROM foods
+                                     WHERE foods.id=?`, [food_id]);
+      return food.rows[0];
+    } catch(error) {
+      return error;
+    }
   }
 }
 
