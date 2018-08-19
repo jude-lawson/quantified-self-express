d0301821b197a6910839a17e04c97ae287206cf9
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 10:33:55 -0600

    Tests Foods#show action for getting a single food

--- test/food_requests.spec.js
+++ test/food_requests.spec.js
@@ -1,6 +1,8 @@
 import chai, { expect } from 'chai';
-import foods from '../fixtures/for_tests/foods'
 import fetch from 'node-fetch'
+
+import foods from '../fixtures/for_tests/foods';
+import food from '../fixtures/for_tests/food';
 
 describe('Food Requests', () => {
   context('GET /api/v1/foods', () => {
@@ -9,6 +11,18 @@ describe('Food Requests', () => {
         .then(response => response.json())
         .then(parsedResponse => {
           expect(parsedResponse).to.deep.eq(foods)
+          done();
+        })
+        .catch(error => console.error(error))
+    });
+  });
+
+  context('GET /api/v1/foods/:id', () => {
+    it('should return a single food with the specified id', done => {
+      fetch('http://localhost:8000/api/v1/foods/1')
+        .then(response => response.json())
+        .then(parsedResponse => {
+          expect(parsedResponse).to.deep.eq(food)
           done();
         })
         .catch(error => console.error(error))
