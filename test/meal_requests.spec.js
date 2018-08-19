ebf8e89e9832d7dfa7b908ddf487f85b0d286242
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 14:01:41 -0600

    Updates meal_requests spec to utilize async/await

--- test/meal_requests.spec.js
+++ test/meal_requests.spec.js
@@ -5,14 +5,12 @@ import meals from '../fixtures/for_tests/meals';
 
 describe('Meal Requests', () => {
   context('GET /api/v1/meals', () => {
-    it('should return all meals in the database', done => {
-      fetch('http://localhost:8000/api/v1/meals')
-        .then(response => response.json())
-        .then(parsedResponse => {
-          expect(parsedResponse).to.deep.eq(meals)
-          done();
-        })
-        .catch(error => console.error(error))
+    it('should return all meals in the database', async () => {
+      let response       = await fetch('http://localhost:8000/api/v1/meals');
+      let parsedResponse = await response.json();
+      
+      expect(response.status).to.eq(200);
+      expect(parsedResponse).to.deep.eq(meals);
     });
   });
 });
