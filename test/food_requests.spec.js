3eb5bc9d7ca457d889e4bda12243cb6592c53b32
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 14:33:22 -0600

    Tests Foods#update endpoint

--- test/food_requests.spec.js
+++ test/food_requests.spec.js
@@ -1,6 +1,7 @@
 import chai, { expect } from 'chai';
 import fetch from 'node-fetch'
 
+
 import foods from '../fixtures/for_tests/foods';
 import food from '../fixtures/for_tests/food';
 
@@ -29,12 +30,13 @@ describe('Food Requests', () => {
     it('given a food object, it should create a food', async () => {
       let new_food   = JSON.stringify({ food: { name: 'Dumplings', calories: 900 } });
       let fetch_init = { method: 'post', body: new_food, headers: { 'Content-Type': 'application/json' } };
+      let expected_food = { id: 5, name: 'Dumplings', calories: 900 }
 
       let response = await fetch('http://localhost:8000/api/v1/foods', fetch_init);
       let parsedResponse = await response.json();
-
+      
       expect(response.status).to.eq(200);
-      expect(parsedResponse).to.deep.eq({ id: 5, name: 'Dumplings', calories: 900 })
+      expect(parsedResponse).to.deep.eq(expected_food);
     })
 
     it('should return 400 if the creation was not successful', async () => {
@@ -66,6 +68,30 @@ describe('Food Requests', () => {
 
       expect(response.status).to.eq(400);
       expect(parsedResponse).to.deep.eq({ error: 'Calories attribute is required' })
+    });
+  });
+
+  context('PATCH /api/v1/foods/:id', () => {
+    it('should update the food with the specified id', async () => {
+      let expected_food     = { food: { name: 'Snickerdoodle', calories: 700 } };
+      let updated_food_data = JSON.stringify(expected_food);
+      let fetch_init        = { method: 'patch', body: updated_food_data, headers: { 'Content-Type': 'application/json' } }
+
+      let response       = await fetch('http://localhost:8000/api/v1/foods/1', fetch_init)
+      let parsedResponse = await response.json();
+
+      expect(response.status).to.eq(200);
+      expect(parsedResponse).to.deep.eq(expected_food);
+    })
+
+    it('should return a 400 if the food update was unsuccessful', async () => {
+      let errant_food       = { food: {} };
+      let updated_food_data = JSON.stringify(errant_food);
+      let fetch_init        = { method: 'patch', body: updated_food_data, headers: { 'Content-Type': 'application/json' } }
+
+      let response = await fetch('http://localhost:8000/api/v1/foods/1', fetch_init)
+
+      expect(response.status).to.eq(400);
     });
   });
 });
