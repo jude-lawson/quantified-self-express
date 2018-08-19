6d00061e23fc54485cbdc0e8371da5dc0d2e937c
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 10:00:35 -0600

    Implements babel build to maintain use of ES6 for production builds (specifically, babel-polyfill and build commands)

--- src/models/Food.js
+++ src/models/Food.js
@@ -1,7 +1,5 @@
-// import ApplicationModel from './ApplicationModel';
-
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
+const configuration = require('../../knexfile')[environment];
 const database = require('knex')(configuration);
 
 class Food  {
