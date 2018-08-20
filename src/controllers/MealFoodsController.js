d65445afba5dd63ec9e5d8eed5411d76fc6c0f41
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 21:39:45 -0600

    Implements MealFood#destroy endpoint

--- src/controllers/MealFoodsController.js
+++ src/controllers/MealFoodsController.js
@@ -5,6 +5,11 @@ class MealFoodsController {
     let result = await MealFood.addFoodToMeal(request.params.meal_id, request.params.id);
     response.status(result.status).json(result.data);
   }
+
+  static async destroy(request, response) {
+    let result = await MealFood.removeFoodFromMeal(request.params.meal_id, request.params.id);
+    response.status(result.status).json(result.data);
+  }
 }
 
 export default MealFoodsController;
