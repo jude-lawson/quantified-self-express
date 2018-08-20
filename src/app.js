39813a12e0190653b265956a0e1a52e41bb8c6e9
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-20 14:04:53 -0600

    Pulls routes out into their own file

--- src/app.js
+++ src/app.js
@@ -4,43 +4,17 @@ const app = express();
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import { environment } from './config';
-
-// const environment = process.env.NODE_ENV || 'development';
-// const configuration = require('../knexfile')[environment];
-// const database = require('knex')(configuration);
-
-import FoodsController from './controllers/foods_controller';
-import MealsController from './controllers/meals_controller';
-import MealFoodsController from './controllers/MealFoodsController';
+import routes from './routes';
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(routes);
 app.set('port', process.env.PORT || 8000);
 app.locals.title = 'Quantified Self Express API';
 
-/* Routes */
 
-// Root
-app.get('/', (request, response) => { response.send('Quantified Self Express API'); });
-
-// Foods
-app.get('/api/v1/foods', (request, response) => { FoodsController.index(request, response) });
-app.get('/api/v1/foods/:id', (request, response) => { FoodsController.show(request, response) });
-app.post('/api/v1/foods', (request, response) => { FoodsController.create(request, response) });
-app.patch('/api/v1/foods/:id', (request, response) => { FoodsController.update(request, response) });
-app.delete('/api/v1/foods/:id', (request, response) => { FoodsController.destroy(request, response) });
-
-// Meals
-app.get('/api/v1/meals', (request, response) => { MealsController.index(request, response); });
-app.get('/api/v1/meals/:meal_id/foods', (request, response) => { MealsController.show(request, response) });
-
-//MealFoods
-app.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => { MealFoodsController.create(request, response) });
-app.delete('/api/v1/meals/:meal_id/foods/:id', (request,response) => { MealFoodsController.destroy(request, response) });
-
-
-/* Open server */
+/* Start server */
 
 app.listen(app.get('port'), () => {
   console.log('Starting server...')
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import FoodsController from './controllers/foods_controller';
+import MealsController from './controllers/meals_controller';
+import MealFoodsController from './controllers/MealFoodsController';
+
+import express from 'express';
+const router = express.Router();
+
+//Root
+router.get('/', (request, response) => { response.send('Quantified Self Express API'); });
+
+// Foods
+router.get('/api/v1/foods', (request, response) => { FoodsController.index(request, response) });
+router.get('/api/v1/foods/:id', (request, response) => { FoodsController.show(request, response) });
+router.post('/api/v1/foods', (request, response) => { FoodsController.create(request, response) });
+router.patch('/api/v1/foods/:id', (request, response) => { FoodsController.update(request, response) });
+router.delete('/api/v1/foods/:id', (request, response) => { FoodsController.destroy(request, response) });
+
+// Meals
+router.get('/api/v1/meals', (request, response) => { MealsController.index(request, response); });
+router.get('/api/v1/meals/:meal_id/foods', (request, response) => { MealsController.show(request, response) });
+
+//MealFoods
+router.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => { MealFoodsController.create(request, response) });
+router.delete('/api/v1/meals/:meal_id/foods/:id', (request,response) => { MealFoodsController.destroy(request, response) });
+
+export default router;
