2494ac079c5af29315e046f23f7201ccf7a5b8a1
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-19 18:54:22 -0600

    Updates tests to have seed occur before eahc test

--- test/food_requests.spec.js
+++ test/food_requests.spec.js
@@ -1,14 +1,61 @@
 import chai, { expect } from 'chai';
 import fetch from 'node-fetch'
 
-import foods from '../fixtures/for_tests/foods';
+import foods from '../fixtures/for_seeds/foods';
+import meals from '../fixtures/for_seeds/meals';
+import meal_foods from '../fixtures/for_seeds/meal_foods';
+
 import food from '../fixtures/for_tests/food';
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = 'test';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 describe('Food Requests', () => {
+  beforeEach(async () => {
+    // Remove all recorsd and reset id sequence all database entries in dependency order
+    await database.raw('DELETE FROM meal_foods')
+    await database.raw('ALTER SEQUENCE meal_foods_id_seq RESTART WITH 1')
+    await database.raw('DELETE FROM foods')
+    await database.raw('ALTER SEQUENCE foods_id_seq RESTART WITH 1')
+    await database.raw('DELETE FROM meals')
+    await database.raw('ALTER SEQUENCE meals_id_seq RESTART WITH 1')
+  
+    // Create foods
+    await database.raw(`INSERT INTO foods (name, calories)
+                        VALUES (?,?)
+                        RETURNING id`, [foods[0].name, foods[0].calories]);
+    await database.raw(`INSERT INTO foods (name, calories)
+                        VALUES (?,?)
+                        RETURNING id`, [foods[1].name, foods[1].calories]);
+    await database.raw(`INSERT INTO foods (name, calories)
+                        VALUES (?,?)
+                        RETURNING id`, [foods[2].name, foods[2].calories]);
+    await database.raw(`INSERT INTO foods (name, calories)
+                        VALUES (?,?)
+                        RETURNING id`, [foods[3].name, foods[3].calories]);
+
+    // Create meals
+    await database.raw(`INSERT INTO meals (name)
+                    VALUES (?)`, [meals[0].name]);
+    await database.raw(`INSERT INTO meals (name)
+                    VALUES (?)`, [meals[1].name]);
+    await database.raw(`INSERT INTO meals (name)
+                    VALUES (?)`, [meals[2].name]);
+    await database.raw(`INSERT INTO meals (name)
+                    VALUES (?)`, [meals[3].name]);
+
+    // Create meal_foods
+    await database.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                    VALUES (?,?)`, [meal_foods[0].meal_id, meal_foods[0].food_id]);
+    await database.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                    VALUES (?,?)`, [meal_foods[1].meal_id, meal_foods[1].food_id]);
+    await database.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                    VALUES (?,?)`, [meal_foods[2].meal_id, meal_foods[2].food_id]);
+    await database.raw(`INSERT INTO meal_foods (meal_id, food_id)
+                    VALUES (?,?)`, [meal_foods[3].meal_id, meal_foods[3].food_id]);
+  })
+
   context('GET /api/v1/foods', () => {
     it('should return all foods in the database', async () => {
       let response       = await fetch('http://localhost:8000/api/v1/foods');
