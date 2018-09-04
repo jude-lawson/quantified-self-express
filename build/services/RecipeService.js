ad61f384a7421c290cef194689451931f5aa44d7
Jude Lawson <lawsonjdutton@gmail.com>
2018-09-04 15:09:56 -0600

    Rebuilds for production to include memoized url parameters for production

--- build/services/RecipeService.js
+++ build/services/RecipeService.js
@@ -56,7 +56,7 @@ var RecipeService = function () {
 
               case 8:
                 _context.next = 10;
-                return (0, _nodeFetch2.default)('http://api.yummly.com/v1/api/recipes?_app_id=' + _secrets.yummly_app_id + '&_app_key=' + _secrets.yummly_app_key + '&q=' + food_name);
+                return (0, _nodeFetch2.default)('http://api.yummly.com/v1/api/recipes?_app_id=' + (_secrets.yummly_app_id || process.env.YUMMLY_APP_ID) + '&_app_key=' + (_secrets.yummly_app_key || process.env.YUMMLY_APP_KEY) + '&q=' + food_name);
 
               case 10:
                 response = _context.sent;
--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -10,7 +10,7 @@ class RecipeService {
     } else if (process.env.NODE_ENV === "test" && food_name === "onion+soup") {
       var parsedResponse = onion_soup_search_yummly
     } else {
-      let response = await fetch(`http://api.yummly.com/v1/api/recipes?_app_id=${yummly_app_id}&_app_key=${yummly_app_key}&q=${food_name}`)
+      let response = await fetch(`http://api.yummly.com/v1/api/recipes?_app_id=${yummly_app_id || process.env.YUMMLY_APP_ID}&_app_key=${yummly_app_key || process.env.YUMMLY_APP_KEY}&q=${food_name}`)
       var parsedResponse = await response.json();
     }
     return await parsedResponse;
